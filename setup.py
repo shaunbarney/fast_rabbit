14edb248bfaa8d531e6f49c38b43038bf8fe6966
shaunbarney <shaunbarney@outlook.com>
2024-03-17 08:49:41 +0000

    :bookmark: V0.1.17

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fast_rabbit",
-    version="0.1.16",
+    version="0.1.17",
     packages=find_packages(),
     install_requires=[
         "aio_pika",
