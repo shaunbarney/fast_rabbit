f80bd6c591477670924e289ef70d787c57f2eb4b
shaunbarney <shaunbarney@outlook.com>
2024-03-17 08:24:24 +0000

    "Bump version: 0.1.14 → 0.1.15"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fast_rabbit",
-    version="0.1.14",
+    version="0.1.15",
     packages=find_packages(),
     install_requires=[
         "aio_pika",
